cf9350ebe8161ca9b56d6a9bc402878cbf132ac2
MD Likhon <mdlikhon0925@gmail.com>
2025-08-05 16:47:19 +0600

    updated files

--- js/script.js
+++ js/script.js
@@ -87,3 +87,15 @@ $(document).ready(function () {
     scrollBtn.fadeOut(); // hide button again
   });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
